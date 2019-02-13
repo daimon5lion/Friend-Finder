f58fff88975795495f4202dc8fccfabbae3b6938
Lei <leichen83@gmail.com>
2019-02-12 20:04:15 -0500

    should be final

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,22 @@
 var friendList = [
   {
     name: "Sam Sun",
-    profilePic: "https://www.meme-arsenal.com/memes/6461043e803d5765fc4c65b1726a3488.jpg",
+    profilePic:
+      "https://github.com/daimon5lion/FriendFinder/blob/master/app/public/images/1.jpg",
     scores: ["3", "5", "5", "3", "4", "5", "4", "4", "3", "4"]
   },
 
   {
     name: "Tim Tony",
-    profilePic: "public/images/2.jpg",
-    scores: ["4", "5", "3", "3", "4", "5", "4", "4", "2", "4"]
+    profilePic:
+      "https://github.com/daimon5lion/FriendFinder/blob/master/app/public/images/2.jpg",
+    scores: ["4", "5", "3", "3", "4", "5", "5", "4", "5", "4"]
   },
   {
     name: "Brian Blue",
-    profilePic: "public/images/3.jpg",
-    scores: ["4", "5", "3", "2", "3", "4", "3", "4", "1", "4"]
+    profilePic:
+      "https://github.com/daimon5lion/FriendFinder/blob/master/app/public/images/3.jpg",
+    scores: ["2", "2", "3", "2", "3", "1", "3", "3", "1", "2"]
   }
 ];
 
