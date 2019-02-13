3aae85f4c70566b9f11e8ae9f03520f0c6b64b5f
Lei <leichen83@gmail.com>
2019-02-12 20:12:05 -0500

    shouldbe final

--- app/data/friends.js
+++ app/data/friends.js
@@ -9,13 +9,13 @@ var friendList = [
   {
     name: "Tim Tony",
     profilePic:
-      "https://www.meme-arsenal.com/memes/6461043e803d5765fc4c65b1726a3488.jpg",
+      "https://static.boredpanda.com/blog/wp-content/uploads/2016/10/funny-dogs-with-eyebrows-26-57f39569397a1__700.jpg",
     scores: ["4", "5", "3", "3", "4", "5", "5", "4", "5", "4"]
   },
   {
     name: "Brian Blue",
     profilePic:
-      "https://www.meme-arsenal.com/memes/6461043e803d5765fc4c65b1726a3488.jpg",
+      "https://static.boredpanda.com/blog/wp-content/uploads/2016/10/funny-dogs-with-eyebrows-36-57f39c8683865-png__700.jpg",
     scores: ["2", "2", "3", "2", "3", "1", "3", "3", "1", "2"]
   }
 ];
