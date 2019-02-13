543f761789039468a166a0b8fac2ea1b785c8471
Lei <leichen83@gmail.com>
2019-02-12 19:59:06 -0500

    test

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendList = [
+  {
+    name: "Sam Sun",
+    profilePic: "https://www.meme-arsenal.com/memes/6461043e803d5765fc4c65b1726a3488.jpg",
+    scores: ["3", "5", "5", "3", "4", "5", "4", "4", "3", "4"]
+  },
+
+  {
+    name: "Tim Tony",
+    profilePic: "public/images/2.jpg",
+    scores: ["4", "5", "3", "3", "4", "5", "4", "4", "2", "4"]
+  },
+  {
+    name: "Brian Blue",
+    profilePic: "public/images/3.jpg",
+    scores: ["4", "5", "3", "2", "3", "4", "3", "4", "1", "4"]
+  }
+];
+
+module.exports = friendList;
